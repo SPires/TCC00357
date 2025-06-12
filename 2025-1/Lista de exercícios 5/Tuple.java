9779079787491ab81b9e72eb0cf5ee49dbf38a00
Maurício Pires <7014930+SPires@users.noreply.github.com>
2025-06-11 21:03:17 -0300

    Create Tuple.java

---
+++ 2025-1/Lista de exercícios 5/Tuple.java
@@ -0,0 +1,9 @@
+public class Tuple<R,S> {
+    public final R first;
+    public final S second;
+
+    public Tuple(R first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+}
